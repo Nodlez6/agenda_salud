643df99622dbb865935563dae6f37bdbaffc9987
Sebastian Andres Gallardo <sebastian.gallardo1@mail.udp.cl>
2022-09-07 13:43:21 -0300

    instalando material ui

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import Button from "@mui/material/Button";
+
+const App = () => {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
-}
+};
 
 export default App;
