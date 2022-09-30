4a050a4573db01e6eb8ae66aa5fd3e057b5c5419
Sebastian Andres Gallardo <sebastian.gallardo1@mail.udp.cl>
2022-09-30 15:48:31 -0300

    cambios

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -1,5 +1,96 @@
 const prisma = require("../prisma/prismaClient");
 
+const formatSchedule = (schedule) => {
+  const Data = {};
+  Data.lunes = [];
+  Data.martes = [];
+  Data.miercoles = [];
+  Data.jueves = [];
+  Data.viernes = [];
+  Data.sabado = [];
+  Data.domingo = [];
+  schedule.forEach((item) => {
+    const date = item.fecha;
+    date.setDate(date.getDate() + 1);
+
+    switch (date.getDay()) {
+      case 1:
+        Data.lunes.push({
+          id: item.id,
+          fecha: date.toLocaleDateString(),
+          desde: item.desde.toLocaleTimeString(),
+          hasta: item.hasta.toLocaleTimeString(),
+          periodicidad: item.periodicidad,
+          deleted_at: item.deletedat,
+        });
+        break;
+      case 2:
+        Data.martes.push({
+          id: item.id,
+          fecha: date.toLocaleDateString(),
+          desde: item.desde.toLocaleTimeString(),
+          hasta: item.hasta.toLocaleTimeString(),
+          periodicidad: item.periodicidad,
+          deleted_at: item.deletedat,
+        });
+        break;
+      case 3:
+        Data.miercoles.push({
+          id: item.id,
+          fecha: date.toLocaleDateString(),
+          desde: item.desde.toLocaleTimeString(),
+          hasta: item.hasta.toLocaleTimeString(),
+          periodicidad: item.periodicidad,
+          deleted_at: item.deletedat,
+        });
+        break;
+      case 4:
+        Data.jueves.push({
+          id: item.id,
+          fecha: date.toLocaleDateString(),
+          desde: item.desde.toLocaleTimeString(),
+          hasta: item.hasta.toLocaleTimeString(),
+          periodicidad: item.periodicidad,
+          deleted_at: item.deletedat,
+        });
+        break;
+      case 5:
+        Data.viernes.push({
+          id: item.id,
+          fecha: date.toLocaleDateString(),
+          desde: item.desde.toLocaleTimeString(),
+          hasta: item.hasta.toLocaleTimeString(),
+          periodicidad: item.periodicidad,
+          deleted_at: item.deletedat,
+        });
+        break;
+      case 6:
+        Data.sabado.push({
+          id: item.id,
+          fecha: date.toLocaleDateString(),
+          desde: item.desde.toLocaleTimeString(),
+          hasta: item.hasta.toLocaleTimeString(),
+          periodicidad: item.periodicidad,
+          deleted_at: item.deletedat,
+        });
+        break;
+      case 0:
+        Data.domingo.push({
+          id: item.id,
+          fecha: date.toLocaleDateString(),
+          desde: item.desde.toLocaleTimeString(),
+          hasta: item.hasta.toLocaleTimeString(),
+          periodicidad: item.periodicidad,
+          deleted_at: item.deletedat,
+        });
+        break;
+      default:
+        break;
+    }
+  });
+  return Data;
+};
+
 const getAllSchedule = async (req, res) => {
   try {
     const schedule = await prisma.horarios.findMany();
@@ -9,8 +100,38 @@ const getAllSchedule = async (req, res) => {
   }
 };
 
+const getScheduleBySpecialistWithPeriodicidad = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const schedule = await prisma.horarios.findMany({
+      where: { id_especialista: Number(id), NOT: { periodicidad: 0 } },
+    });
+
+    const data = formatSchedule(schedule);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getScheduleBySpecialistWithoutPeriodicidad = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const schedule = await prisma.horarios.findMany({
+      where: { id_especialista: Number(id), periodicidad: 0 },
+    });
+
+    const data = formatSchedule(schedule);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const createSchedule = async (req, res) => {
   const { idEspecialista, fecha, horarios, periodicidad } = req.body;
+
+  const deletedDate = req.body.deleted_at;
 
   let schedule;
   try {
@@ -22,6 +143,7 @@ const createSchedule = async (req, res) => {
           desde: new Date(item.desde),
           hasta: new Date(item.hasta),
           periodicidad: periodicidad,
+          deletedat: deletedDate,
         },
       });
     });
@@ -31,7 +153,22 @@ const createSchedule = async (req, res) => {
   }
 };
 
+const deleteSchedule = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const schedule = await prisma.horarios.delete({
+      where: { id: Number(id) },
+    });
+    res.status(200).json(schedule);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllSchedule,
   createSchedule,
+  getScheduleBySpecialistWithPeriodicidad,
+  getScheduleBySpecialistWithoutPeriodicidad,
+  deleteSchedule,
 };
--- backend/routes/schedulesRouter.js
+++ backend/routes/schedulesRouter.js
@@ -5,8 +5,14 @@ const router = express.Router();
 const {
   getAllSchedule,
   createSchedule,
+  getScheduleBySpecialistWithPeriodicidad,
+  getScheduleBySpecialistWithoutPeriodicidad,
+  deleteSchedule,
 } = require("../controllers/scheduleController");
 
 router.route("/").get(getAllSchedule).post(createSchedule);
+router.route("/:id").delete(deleteSchedule);
+router.route("/with/:id").get(getScheduleBySpecialistWithPeriodicidad);
+router.route("/without/:id").get(getScheduleBySpecialistWithoutPeriodicidad);
 
 module.exports = router;
